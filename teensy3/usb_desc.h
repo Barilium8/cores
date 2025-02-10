6a8485fef7792653bfe8280baac53b0bce83e259
Blake Bender <ekalbredneb@gmail.com>
2025-02-10 07:40:17 -0800

    Finalize string descriptor editing at startup.  Also fix defines for building MRCC, since we had been using two different ones and needed to agree on one.

--- teensy3/usb_desc.h
+++ teensy3/usb_desc.h
@@ -115,44 +115,7 @@ If these instructions are missing steps or could be improved, please
 let me know?  http://forum.pjrc.com/forums/4-Suggestions-amp-Bug-Reports
 */
 
-#if defined(MRCC_USB_MIDI12_SERIAL)
-  #define VENDOR_ID		    0x16C0   // via "VOTI" PJRC USB Vendor PJRC PID=1140-1239 dec
-  #define PRODUCT_ID		  0x0490 // test  MRCC 0x0490(1168), NDLR is 0x0489(1167) 	
-  #define BCD_DEVICE		  0x0213
-  #define MANUFACTURER_NAME	{'T','e','e','n','s','y','d','u','i','n','o'}
-  #define MANUFACTURER_NAME_LEN	11
-  #define PRODUCT_NAME		{'T','e','e','n','s','y',' ','M','I','D','I','x','1','6'}
-  #define PRODUCT_NAME_LEN	14
-  #define EP0_SIZE		64
-  #define NUM_ENDPOINTS         5
-  #define NUM_USB_BUFFERS	30
-  #define NUM_INTERFACE		3
-  #define CDC_IAD_DESCRIPTOR	1
-  #define CDC_STATUS_INTERFACE	0
-  #define CDC_DATA_INTERFACE	  1   // Serial
-  #define CDC_ACM_ENDPOINT	1
-  #define CDC_RX_ENDPOINT       2
-  #define CDC_TX_ENDPOINT       3
-  #define CDC_ACM_SIZE          16
-  #define CDC_RX_SIZE           64
-  #define CDC_TX_SIZE           64
-  #define MIDI_INTERFACE        2   // MIDI
-  #define MIDI_NUM_CABLES       12  // Conductive Labs (was 16)
-  #define MIDI_TX_ENDPOINT      4
-  #define MIDI_TX_SIZE          64
-  #define MIDI_RX_ENDPOINT      5
-  #define MIDI_RX_SIZE          64
-//  #define KEYBOARD_INTERFACE    3	// Keyboard
-//  #define KEYBOARD_ENDPOINT     4
-//  #define KEYBOARD_SIZE         8
-//  #define KEYBOARD_INTERVAL     1
-  #define ENDPOINT1_CONFIG	ENDPOINT_TRANSMIT_ONLY
-  #define ENDPOINT2_CONFIG	ENDPOINT_RECEIVE_ONLY
-  #define ENDPOINT3_CONFIG	ENDPOINT_TRANSMIT_ONLY
-  #define ENDPOINT4_CONFIG	ENDPOINT_TRANSMIT_ONLY
-  #define ENDPOINT5_CONFIG	ENDPOINT_RECEIVE_ONLY
-
-#elif defined(USB_SERIAL)
+#if defined(USB_SERIAL)
   #define VENDOR_ID		0x16C0
   #define PRODUCT_ID		0x0483
   #define DEVICE_CLASS		2	// 2 = Communication Class
@@ -516,7 +479,7 @@ let me know?  http://forum.pjrc.com/forums/4-Suggestions-amp-Bug-Reports
 
 #elif defined(USB_MIDI16)
   #define VENDOR_ID		0x16C0
-  #define PRODUCT_ID		0x0540 // test  MRCC 0x0490(1168), NDLR is 0x0489(1167) 	
+  #define PRODUCT_ID		0x0490 // MRCC 0x0490(1168), NDLR is 0x0489(1167) 	
   #define BCD_DEVICE		0x0212
   #define MANUFACTURER_NAME	{'T','e','e','n','s','y','d','u','i','n','o'}
   #define MANUFACTURER_NAME_LEN	11
@@ -622,7 +585,7 @@ let me know?  http://forum.pjrc.com/forums/4-Suggestions-amp-Bug-Reports
   // **************************************************************************************/
 
   #define VENDOR_ID	  	0x16C0 // via "VOTI" PJRC USB Vendor 
-  #define PRODUCT_ID	0x047E // test  MRCC 0x0490(1150), PJRC PID=1140-1239 dec  FYI NDLR is 0x0489(1167) 	
+  #define PRODUCT_ID	0x0490 // MRCC 0x0490(1150), PJRC PID=1140-1239 dec  FYI NDLR is 0x0489(1167) 	
   #define BCD_DEVICE	0x0212
   /* ***************************************************************************************
   //   NOTE:  FYI These strings are modified in file  USBNAMES.C  in C:\Github\MRCC\src\
